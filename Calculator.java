6002ce6a7ba72c86f7c06d1e823af3e489aeb08b
vivek <vivekthegamer999@gmail.com>
2025-01-07 13:06:17 +0530

    first commit

--- Calculator.java
+++ Calculator.java
@@ -1,5 +1,6 @@
-class first{
+import java.util.*;
+public class Calculator{
     public static void main(String args[]){
-        System.out.println("helll world");
+        int x = 19, y = 80;
     }
 }
