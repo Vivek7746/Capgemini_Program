25a9de0548b19d00fb38379e0705fa10d7460db2
vivek <vivekthegamer999@gmail.com>
2025-01-07 14:57:35 +0530

    addition function

--- Calculator.java
+++ Calculator.java
@@ -1,6 +1,13 @@
 import java.util.*;
 public class Calculator{
+
+    public static int addition(int x, int y)
+    {
+        return x+y;
+    }
+
     public static void main(String args[]){
         int x = 19, y = 80;
+        addition(x, y);
     }
 }
